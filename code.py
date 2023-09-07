966a0424c0afbebaf07831f930bf8e3d6e99ab88
Daniel <danielcoco96@gmail.com>
2023-09-07 20:38:55 +0200

    Gerber and PCB design files

--- code.py
+++ code.py
@@ -5,26 +5,45 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-# Create a list of keycodes (empty in this example)
-key_layout = []
+# Define GPIO pins
+button_pins = [
+    board.GP0,
+    board.GP1,
+    board.GP2,
+    board.GP3,
+    board.GP4,
+    board.GP6,
+    board.GP7,
+    board.GP26,
+    board.GP27
+]
+# Define keycodes
+keycodes = [Keycode.A, Keycode.B, Keycode.C, Keycode.D, Keycode.E, Keycode.F, Keycode.G, Keycode.H]
 
-# Create a keyboard object
+# Button init
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    
+button_state = {i: False for i in range(len(button_pins))}
 keyboard = Keyboard(usb_hid.devices)
-# Define the LED pinaaaaaaa
-led_pin = board.LED
 
-button_pin = board.GP5
-button = digitalio.DigitalInOut(button_pin)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
-
-print("Hello world")
+print("MacroPad initialized")
 
 while True:
-    if not button.value:
-        print("Button pressed!")
-        keyboard.press(Keycode.A)
-        keyboard.release_all()
-    
-    # Add a small delay to avoid rapid button presses
-    time.sleep(0.1)
+    for i, button in enumerate(buttons):
+        if not button.value:
+            # Pressed
+            if not button_state[i]:
+                #keyboard.press(keycodes[i])
+                print(f"Button {button_pins[i]} pressed")
+                button_state[i] = True
+        else:
+            # Released
+            if button_state[i]:
+                #keyboard.release(keycodes[i])
+                print(f"Button {button_pins[i]} released")
+                button_state[i] = False
+
+    time.sleep(0.05)
